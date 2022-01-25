c02ee1a83863f46a38dab81fc09b446a1a860b7a
Nicolas Barlogis <nicolas.barlogis@cdbdx.biz>
2022-01-25 17:00:12 +0100

    feat(statement): ensure correct params

--- src/Statement.js
+++ src/Statement.js
@@ -4,6 +4,16 @@ export default class Statement {
   #balance
 
   constructor(date, amount, balance) {
+  	if(!(date instanceof Date)) {
+  		throw new TypeError("date must be a Date")
+  	}
+  	if(isNaN(amount)) {
+  		throw new TypeError("amount must be a number")
+  	}
+  	if(isNaN(balance)) {
+  		throw new TypeError("balance must be a number")
+  	}
+
     this.#date = date
     this.#amount = amount
     this.#balance = balance
--- src/tests/Statement.test.js
+++ src/tests/Statement.test.js
@@ -2,8 +2,20 @@ import Statement from '../Statement'
 
 test('Statement Should return correct balance', () => {
   // Arrange
-  const statement = new Statement(Date.now(), 50, 120)
+  const statement = new Statement(new Date(Date.now()), 50, 120)
 
   // Assert
   expect(statement.getBalance()).toBe(120)
+});
+
+test('Statement Should have correct date', () => {
+   expect(() => { new Statement("toto", 50, 120) }).toThrow(TypeError)
+});
+
+test('Statement Should have correct amount', () => {
+   expect(() => { new Statement(new Date(Date.now()), "toto", 120) }).toThrow(TypeError)
+});
+
+test('Statement Should have correct balance', () => {
+   expect(() => { new Statement(new Date(Date.now()), 50, "toto") }).toThrow(TypeError)
 });
