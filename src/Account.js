12aa82f62638637916cecccd7c7daa943619e254
nbarlogis <nicolasbarlogis@gmail.com>
2022-01-26 15:17:39 +0100

    fix(account): merge support float values Nba/fix float amount (#4) * fix(account): support float values * test(account): updated tests to verify float values Co-authored-by: Nicolas Barlogis <nicolas.barlogis@cdbdx.biz> (cherry picked from commit bf527551fae1996c3372375533e1e50137271ba5)

--- src/Account.js
+++ src/Account.js
@@ -5,21 +5,22 @@ export default class Account {
     this.#balance = 0
   }
 
+  // 2 digits precision
   getBalance() {
-    return this.#balance
+    return parseFloat(this.#balance.toFixed(2))
   }
 
   deposit(amount) {
     if(isNaN(amount) || amount < 0) {
       throw new TypeError("Must be a number")
     }
-    this.#balance += Math.round(amount)
+    this.#balance += amount
   }
 
   withdraw(amount) {
     if(isNaN(amount) || amount < 0) {
       throw new TypeError("Must be a number")
     }
-    this.#balance -= Math.round(amount)
+    this.#balance -= amount
   }
 }
--- src/tests/Account.test.js
+++ src/tests/Account.test.js
@@ -16,12 +16,12 @@ test('Account with multiple deposits Should have correct balance', () => {
   const account = new Account()
   
   // Act
-  account.deposit(50)
-  account.deposit(150)
-  account.deposit(30)
+  account.deposit(50.7)
+  account.deposit(150.68)
+  account.deposit(30.26)
 
   // Assert
-  expect(account.getBalance()).toBe(230)
+  expect(account.getBalance()).toBe(231.64)
 });
 
 test('Deposits Should only accept numbers', () => {
@@ -57,11 +57,11 @@ test('Account with multiple withdrawal Should have correct balance', () => {
   
   // Act
   account.withdraw(50)
-  account.withdraw(150)
-  account.withdraw(30)
+  account.withdraw(150.3)
+  account.withdraw(30.9)
 
   // Assert
-  expect(account.getBalance()).toBe(-230)
+  expect(account.getBalance()).toBe(-231.2)
 });
 
 test('Withdraws Should only accept numbers', () => {
@@ -78,4 +78,20 @@ test('Withdraws Should only accept positive numbers', () => {
 
   // Act/Assert
   expect(() => { account.withdraw(-10) }).toThrow(TypeError)
+});
+
+test('Account with multiple withdrawal/deposits Should have correct balance', () => {
+  // Arrange
+  const account = new Account()
+  
+  // Act
+  account.deposit(50)
+  account.deposit(36.3)
+  account.withdraw(60)
+  account.withdraw(110.75)
+  account.deposit(105)
+  account.withdraw(27.03)
+
+  // Assert
+  expect(account.getBalance()).toBe(-6.48)
 });
